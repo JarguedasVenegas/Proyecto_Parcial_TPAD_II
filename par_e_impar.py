e8abbf03c92deed9bb952bca88f11585cede7f52
JarguedasVenegas <jarguedasvenegas@gmail.com>
2024-07-02 22:50:08 -0600

    programa en python para saber si es par o impar el numero agregado

---
+++ par_e_impar.py
@@ -0,0 +1,9 @@
+# Solicitar un número al usuario
+numero = int(input("Ingrese un número: "))
+
+# Determinar si el número es par o impar
+if numero % 2 == 0:
+    print(numero," es un número par")
+else:
+    print(numero ,"es un número impar")
+
